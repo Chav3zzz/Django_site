118e7d18069a635ca26378248c9ce17808e0c635
Antonio Chavez <theantoniochavez@gmail.com>
2016-09-08 16:13:05 -0700

    Database API working, Template linked up, Data linked.

---
+++ unchained/music/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Album
+
+def index(request):
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
+
+def info(request, album_id):
+    return HttpResponse("<h2>Album id: " + str(album_id) + "</h2>")
+
+
+
+    # html = ''
+    # for album in all_albums:
+    #     url = '/music/' + str(album.id) + '/'
+    #     html += '<a href="' + url + '">' + album.album_title + '</a><br>'
