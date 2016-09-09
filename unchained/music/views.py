0de9e944c397057e2041eb354979145c3e2228dc
Antonio Chavez <theantoniochavez@gmail.com>
2016-09-08 18:54:32 -0700

    Template connected to database. Refactored some code.

--- unchained/music/views.py
+++ unchained/music/views.py
@@ -1,21 +1,11 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
 
 def info(request, album_id):
-    return HttpResponse("<h2>Album id: " + str(album_id) + "</h2>")
-
-
-
-    # html = ''
-    # for album in all_albums:
-    #     url = '/music/' + str(album.id) + '/'
-    #     html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/info.html', {'album': album})
